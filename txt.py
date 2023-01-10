77afcd2426d219552dbb7d8d3411ec8eab533fbd
LiquidIce95 <114932597+LiquidIce95@users.noreply.github.com>
2023-01-10 19:52:56 +0100

    Update txt.py

--- txt.py
+++ txt.py
@@ -1,13 +1,13 @@
 
 
-file1 = open("/media/Dave/Volume/Users/david/Desktop/Pay2Win/DB/Log.txt","a")
+file1 = open("/media/usr/Volume/Users/usr/Desktop/Pay2Win/DB/Log.txt","a")
 
 file1.writelines("\ntest")
 
 file1.close()
 
 
-file1 = open("/media/Dave/Volume/Users/david/Desktop/Pay2Win/DB/Log.txt","r")
+file1 = open("/media/usr/Volume/Users/usr/Desktop/Pay2Win/DB/Log.txt","r")
 
 print(file1.readlines())
 
