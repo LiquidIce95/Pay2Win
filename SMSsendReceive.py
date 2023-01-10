b950345cb656b50d6f20385be93c4497670812f5
LiquidIce95 <114932597+LiquidIce95@users.noreply.github.com>
2023-01-10 19:46:46 +0100

    Update SMSsendReceive.py

--- SMSsendReceive.py
+++ SMSsendReceive.py
@@ -6,7 +6,7 @@ auth_token = "fa67cfde624ee75da423a370f945afe9"
 
 client = Client(account_sid, auth_token)
 k = 0
-#client.messages.create(to="+41764409375", from_="+12058783722", body="******Pay2Win********")
+#client.messages.create(to="+00000", from_="+12058783722", body="******Pay2Win********")
 for sms in client.messages.list():
     print(sms.to)
     print(sms.from_)
